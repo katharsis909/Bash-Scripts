c61b1f9007f4f5f3b877c3578bfd408638f54e7b
Nirvana <112411201+HachimanNirvana@users.noreply.github.com>
2022-09-01 14:45:04 +0530

    Create Navigate.sh

---
+++ Navigate.sh
@@ -0,0 +1,11 @@
+#changing directory to documents
+$ cd documents
+
+#new directory under 'documents'
+$ mkdir assignments
+#navigating again
+$ cd assignments
+
+
+#to print directories and files under current directory
+$ ls 

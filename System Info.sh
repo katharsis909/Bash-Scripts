8eca85138e0c61a6a62867be2cb3ec6694adbb9c
Nirvana <112411201+HachimanNirvana@users.noreply.github.com>
2022-09-04 15:43:04 +0530

    Create System Info.sh

---
+++ System Info.sh
@@ -0,0 +1,12 @@
+#note that most of them not work on macOS
+
+#Brightness
+xrandr --output LVDS1 --brightness [value]
+
+#battery info
+grep -E "state|to\ full|percentage"
+
+#cpu temp.
+sudo apt-get install lm-sensors #installs the sensors
+sudo sensors-detect
+$ sensors | grep -oP 'Physical.*?\+\K[0-9.]+'

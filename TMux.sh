0bfaf844c2db842d60f835729e076dd0a2d24928
Nirvana <112411201+HachimanNirvana@users.noreply.github.com>
2022-09-04 16:17:34 +0530

    Create TMux.sh

---
+++ TMux.sh
@@ -0,0 +1,11 @@
+tmux starts a new session.
+tmux new -s NAME starts it with that name.
+tmux ls lists the current sessions
+Within tmux typing <C-b> d detaches the current session
+
+<C-b> c Creates a new window. To close it you can just terminate the shells doing <C-d>
+<C-b> N Go to the N th window. Note they are numbered
+<C-b> p Goes to the previous window
+<C-b> n Goes to the next window
+<C-b> , Rename the current window
+<C-b> w List current windows

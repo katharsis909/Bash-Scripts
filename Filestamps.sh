b14f020b70f138f34dfe1000ad7d79ed292a3cfb
Nirvana <112411201+HachimanNirvana@users.noreply.github.com>
2022-09-04 16:01:24 +0530

    Update Filestamps.sh

--- Filestamps.sh
+++ Filestamps.sh
@@ -1,1 +1,10 @@
+#to view filestamps
+stat -f "%Sm" -t "%Y-%m-%d %H:%M" [filename]
 
+#for a range of files
+for i in /var/log/*.out; do
+  stat -f "%Sm" -t "%Y-%m-%d %H:%M" "$i"
+done
+
+#edit filestamps
+touch -r "file" -d '+8 hour' "file" #adds 8 hour to file's entry date

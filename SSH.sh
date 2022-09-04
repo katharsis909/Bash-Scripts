cece476e4f79f6595f3caf709b7fa0b189991550
Nirvana <112411201+HachimanNirvana@users.noreply.github.com>
2022-09-04 16:23:55 +0530

    Create SSH.sh

---
+++ SSH.sh
@@ -0,0 +1,7 @@
+#To ssh(secure shell) into a server you execute a command as follows
+ssh foo@bar.mit.edu
+
+scp when copying large amounts of files/directories, 
+the secure copy scp command is more convenient since it can easily recurse over paths. 
+The syntax is scp path/to/local_file remote_host:path/to/remote_file
+

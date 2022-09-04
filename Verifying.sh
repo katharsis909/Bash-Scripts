551c9d3289af226c9ad2338b7821b6f9b317fe85
Nirvana <112411201+HachimanNirvana@users.noreply.github.com>
2022-09-04 16:18:59 +0530

    Create Verifying.sh

---
+++ Verifying.sh
@@ -0,0 +1,7 @@
+if [[ "$(uname)" == "Linux" ]]; then {do_something}; fi
+
+# Check before using shell-specific features
+if [[ "$SHELL" == "zsh" ]]; then {do_something}; fi
+
+# You can also make it machine-specific
+if [[ "$(hostname)" == "myServer" ]]; then {do_something}; fi

b62354faf1bb5f2d79b1e2771786b8f3f5388d6a
Nirvana <112411201+HachimanNirvana@users.noreply.github.com>
2022-09-04 15:46:57 +0530

    Create Code.sh

---
+++ Code.sh
@@ -0,0 +1,14 @@
+#if else example
+if [ 15 -gt 10 ]
+then  
+# If variable less than 10  
+   echo "a is greater than 10"
+
+fi 
+
+#for loop
+n=7
+for (( i=1 ; i<=$n ; i++ )); 
+do
+    echo $i
+done
